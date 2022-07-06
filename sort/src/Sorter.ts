deef65137eda8eefdfeb153348d16360ac259b0d
davis <davisochieng4@gmail.com>
2022-07-06 11:20:47 -0400

    Refactor to use Abstract classes and class inheritance; comparing interfaces and inheritance for code reuse

--- sort/src/Sorter.ts
+++ sort/src/Sorter.ts
@@ -1,18 +1,14 @@
-interface Sortable {
-  length: number;
-  compare: (i: number, j: number) => boolean;
-  swap: (i: number, j: number) => void;
-}
-
-export class Sorter {
-  constructor(public collection: Sortable) {}
+export abstract class Sorter {
+  abstract length: number;
+  abstract compare(i: number, j: number): boolean;
+  abstract swap(i: number, j: number): void;
 
   sort = (): void => {
-    const { length } = this.collection;
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection.compare(j, j + 1)) {
-          this.collection.swap(j, j + 1);
+    const { length } = this;
+    for (let i = 0; i < this.length; i++) {
+      for (let j = 0; j < this.length - i - 1; j++) {
+        if (this.compare(j, j + 1)) {
+          this.swap(j, j + 1);
         }
       }
     }
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,11 +1,11 @@
 import { NumbersCollection } from './NumbersCollection';
-import { Sorter } from './Sorter';
+
 import { CharactersCollection } from './CharactersCollection';
 
-// const numbersCollection = new NumbersCollection([12, 34, 1, 34, 15, 8, 1000]);
-// sorter.sort();
-// console.log(numbersCollection.data);
+const numbersCollection = new NumbersCollection([12, 34, 1, 34, 15, 8, 1000]);
+numbersCollection.sort();
+console.log(numbersCollection.data);
 const characterCollection = new CharactersCollection('XaayBbbc');
-const sorter = new Sorter(characterCollection);
-sorter.sort();
+characterCollection.sort();
 console.log(characterCollection.data);
+console.log('s');
